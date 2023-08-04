3ef2a052984e168e13f2a85a6951cf7cf6f31934
xavierrice <xavier.rice@gmail.com>
2023-08-04 14:05:49 -0400

    added event handlers for all selections

--- src/Components/ClassList.jsx
+++ src/Components/ClassList.jsx
@@ -1,17 +1,22 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./ClassList.css";
 
 const ClassList = ({ data }) => {
- 
+
    const [selection, setSelection] = useState("")
    console.log(selection)
-  function handleSeason(event){
 
-  const splitWords =event.target.innerText.split(" ")
-  const theSelection = `${splitWords[0]}${splitWords[1]}`
- 
-    setSelection(theSelection)
-  }
+   function handleSeason(event) {
+
+      const splitWords = event.target.innerText.split(" ")
+      const theSelection = `${splitWords[0]}${splitWords[1]}`
+      setSelection(theSelection)
+   }
+
+ function handleAll(event){
+    setSelection("All")
+
+ }
 
    const classesList = data.slice().map((student) => student.cohort.cohortCode).filter((season, index, self) => self.indexOf(season) === index) //slicing so i don't mutate the object.maping though to select the season and then filtering out the unique ones.
    const spacedList = classesList.map((strings) => strings.replace(/([a-zA-Z]+)(\d+)/, "$1 $2")) //using RegEx to sort the letters ([a-zA-Z]+) from the digits (\d+) and seperating them one and two
@@ -20,18 +25,17 @@ const ClassList = ({ data }) => {
    const list = sortedList.map((season, index) => {    //taking the sorted list and created indv. <h3>tags for each.
       return (
          <>
-            <div className="ClassCard" onClick={handleSeason} >
+            <div className="ClassCard" onClick={handleSeason} key={index} >
                <hr size="3" width="90%" color="green" />
                <h3 key={index} value={season} >{season}</h3>
             </div>
-
          </>
       )
    });
 
    return (
-      <div className="container" >
-         <div className="ClassList">
+      <div className="container" key="classBox" >
+         <div className="ClassList" onClick={handleAll} >
             <h3>All Students</h3>
             {list}
          </div>
